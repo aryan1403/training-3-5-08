7599e3496577387a64fd9d305fd1cc63ee0b92c1
aryan1403 <aaryan14032006@email.com>
2022-08-03 13:15:44 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setcount] = useState(0);
+  const onbtnclick = () => setcount(count+1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p>{count}</p>
+    <button id='mybtn' onClick={onbtnclick}>Click Me</button>
+    </>
   );
 }
 
---
+++ src/c.js
@@ -0,0 +1,8 @@
+import './c.css';
+export default function C(props) {
+    return (
+        <>
+        <h1 id="h">Welcome {props.name}</h1>
+        </>
+    );
+}
